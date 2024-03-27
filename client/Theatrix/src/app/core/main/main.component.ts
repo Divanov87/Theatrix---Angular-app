8dc8fae86d59afb4a87aba45f579e19462ee5b2c
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-03-27 14:45:27 +0200

    Added Icons for Event Card and Home page

--- client/Theatrix/src/app/core/main/main.component.ts
+++ client/Theatrix/src/app/core/main/main.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+  title = 'Theatrix'
 
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    const searchInput = document.getElementById('search-input');
+    if (searchInput) {
+      this.renderer.selectRootElement(searchInput).focus();
+    }
+  }
 }
+
